427c2749e697f0a19955343ccd1a5ff79d63e583
Güray Gül <guraygul59@gmail.com>
2024-02-01 18:06:55 +0300

    Initial Commit

---
+++ UIKitMoonshot/Controller/MoonshotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoonshotViewController.swift
+//  UIKitMoonshot
+//
+//  Created by Güray Gül on 1.02.2024.
+//
+
+import UIKit
+
+class MoonshotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIKitMoonshot/Model/Moonshot.swift
@@ -0,0 +1,8 @@
+//
+//  Moonshot.swift
+//  UIKitMoonshot
+//
+//  Created by Güray Gül on 1.02.2024.
+//
+
+import Foundation
